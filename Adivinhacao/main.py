14efdaa321aafc5e5bd3b11a7678a40d42945c7c
Pedro Augusto de Portilho Ronzani <pedroaugustogabironzani@hotmail.com>
2023-01-10 23:14:57 -0300

    jogos

--- Adivinhacao/main.py
+++
@@ -1,4 +0,0 @@
-import adivinhacao
-
-if __name__ == '__main__':
-    adivinhacao.jogoAdivinhacao()
---
+++ Jogos/main.py
@@ -0,0 +1,5 @@
+import adivinhacao
+
+if __name__ == '__main__':
+    match int(input("1) Jogos\n2) Forca\nEscolha: ")):
+        case 1: adivinhacao.jogoAdivinhacao()
