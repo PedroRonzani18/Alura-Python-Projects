76de916086da0911397e1a50ebfedaf8d40b20ea
Pedro Augusto de Portilho Ronzani <pedroaugustogabironzani@hotmail.com>
2023-02-04 01:30:24 -0300

    arrumando

--- data_science/paths/src/main.py
+++ data_science/paths/src/main.py
@@ -1,19 +1,11 @@
 from pathlib import Path
 
-import pandas as pd
-
 
 def partial_to_full_path(path):
     parent = Path(__file__).parent
     for _ in range(path.count('../')):
         parent = parent.parent
-    parent = str(parent)
-
-    barras = path.count('../') * '../'
-    parent += path[path.find(barras) + len(barras) - 1:]
-    return parent
+    return parent.joinpath(path.lstrip('../'))
 
 
-dados = pd.read_csv(partial_to_full_path("../csv/aluguel.csv"), sep=';')
-
-print(dados)
+print(partial_to_full_path("../csv/aluguel.csv"))
